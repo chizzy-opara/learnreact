487ef6eab570d87bc0277ab60f28742c8ee6e071
chizzy-opara <chizitereopara@gmail.com>
2023-10-27 06:16:12 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+// Bootstrap CSS to apply the styles
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        {/* Bootstrap Navbar component */}
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand>GOMYCODE REACT JS CHECKPOINT</Navbar.Brand>
+          </Container>
+        </Navbar>
+
+        <Container>
+          {/* Heading using an h1 element */}
+          <h1 className="mt-5">Welcome to My React App</h1>
+
+          <Row className="mt-4">
+            {/* A Row to contain three Col components */}
+            <Col>
+              {/* Bootstrap Card component for the card 1*/}
+              <Card>
+                <Card.Body>
+                  <Card.Title>Card 1</Card.Title>
+                  <Card.Text>This is the first card.</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col>
+              {/* Bootstrap Card component for the card 2*/}
+              <Card>
+                <Card.Body>
+                  <Card.Title>Card 2</Card.Title>
+                  <Card.Text>This is the second card.</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col>
+              {/* Bootstrap Card component for the card 3*/}
+              <Card>
+                <Card.Body>
+                  <Card.Title>Card 3</Card.Title>
+                  <Card.Text>This is the third card.</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
